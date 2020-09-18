d3cfbdd5dfb992fa9cdd1d56346635c3d313d677
hp0404 <hryhorii.pavlenko@gmail.com>
2020-09-18 11:01:44 +0300

    Перейменував main в async

--- async.py
+++ async.py
@@ -1,10 +1,7 @@
 import json
-import random
-import logging
 import asyncio
 import aiohttp
 import pandas as pd
-from datetime import datetime
 
 URL = "https://spending.gov.ua/portal-api/v2/api/transactions/page/" 
 DATE = "2020-09-16"
@@ -13,8 +10,11 @@ CONCURRENT_CONNECTIONS = 5
 
 
 async def fetch(sem, session, page):
-    params = {"page": page, "pageSize": 100,"startdate": DATE, "enddate": DATE}
-    async with sem, session.get(URL, params=params, raise_for_status=True) as response:
+    params = {
+        "page": page, "pageSize": 100, 
+        "startdate": DATE, "enddate": DATE
+    }
+    async with sem, session.get(URL, params=params) as response:
         data = await response.json()
         return {"date": DATE, "page": page, "raw_json": json.dumps(data)}
 
@@ -31,8 +31,8 @@ async def fetch_all(loop):
 def main():
     loop = asyncio.get_event_loop()
     data = loop.run_until_complete(fetch_all(loop))
-    data.to_csv(f"data/sample.csv", index=False)
- 
-
+    data.to_csv("data/sample.csv", index=False)
+    
+    
 if __name__ == "__main__":
-   main()
+    main()
