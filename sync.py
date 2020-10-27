ff2d11b1f81053f1c0965ceeb96ca31e6c3dd2ae
hp0404 <hryhorii.pavlenko@gmail.com>
2020-10-27 09:57:38 +0200

    refactor after code review: hooks, params creation, writing to csv on the fly

--- sync.py
+++ sync.py
@@ -1,28 +1,54 @@
 import requests
 import pandas as pd
+from pathlib import Path
 from datetime import date, timedelta
+from typing import Any, Dict, Iterator
 
 
-DATE = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+DATE = date.today() - timedelta(days=1)
+PATH = Path(__file__).resolve().parent / "data"
 URL = "https://spending.gov.ua/portal-api/v2/api/transactions/page/" 
 
 
-def fetch(session, params):
-    next_page, last_page = 0, 0
+def to_dataframe(json_data: Dict[str, Any], page: int) -> pd.DataFrame:
+    return pd.json_normalize(json_data["transactions"]).assign(page=page)
+
+
+def fetch(
+    session: requests.Session,
+    start_date: date,
+    end_date: date,
+    starting_page: int = 0,
+    page_size: int = 100,
+) -> Iterator[pd.DataFrame]:
+    params = {
+        "startdate": start_date.isoformat(),
+        "enddate": end_date.isoformat(),
+        "page": starting_page,
+        "pageSize": page_size,
+    }
+    next_page, last_page = starting_page, starting_page
     while next_page <= last_page:
         params["page"] = next_page
         data = session.get(URL, params=params).json()
-        yield pd.json_normalize(data.get("transactions"))\
-                .assign(page=params.get("page"))
+        yield to_dataframe(data, next_page)
         next_page, last_page = next_page+1, data["count"] // data["pageSize"]
                 
         
-def fetch_all():
+def fetch_all() -> Iterator[pd.DataFrame]:
     with requests.Session() as session:
-        params = {"page": 0, "pageSize": 100, "startdate": DATE, "enddate": DATE}
-        yield from fetch(session, params)
+        session.hooks["response"].append(
+            lambda r, *args, **kwargs: r.raise_for_status()
+        )
+        yield from fetch(session, start_date=DATE, end_date=DATE)
         
         
 if __name__ == "__main__":
+    output_path = PATH / f"{DATE}.csv"
+    output_path.unlink(missing_ok=True)
     data = fetch_all()
-    pd.concat(data).to_csv(f"data/{DATE}.csv", index=False)
+    for i, dataframe in enumerate(data):
+        write_header = True if i == 0 else False
+        dataframe.to_csv(
+            output_path, header=write_header, index=False, mode="a"
+        )
