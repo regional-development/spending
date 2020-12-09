0f77550f51cc45590987a49e6c12c4572150f784
Hryhorii Pavlenko <39014459+hp0404@users.noreply.github.com>
2020-12-09 13:00:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from datetime import date, timedelta
 from typing import Any, Dict, Iterator
 
 
-DATE = date.today() - timedelta(days=2)
+DATE = date.today() - timedelta(days=1)
 PATH = Path(__file__).resolve().parent / "data"
-URL = "https://spending.gov.ua/portal-api/v2/api/transactions/page/" 
+URL = "https://spending.gov.ua/portal-api/v2/api/transactions/page/"
 
 
 def to_dataframe(json_data: Dict[str, Any], page: int) -> pd.DataFrame:
@@ -32,23 +32,21 @@ def fetch(
         params["page"] = next_page
         data = session.get(URL, params=params).json()
         yield to_dataframe(data, next_page)
-        next_page, last_page = next_page+1, data["count"] // data["pageSize"]
-                
-        
+        next_page, last_page = next_page + 1, data["count"] // data["pageSize"]
+
+
 def fetch_all() -> Iterator[pd.DataFrame]:
     with requests.Session() as session:
         session.hooks["response"].append(
             lambda r, *args, **kwargs: r.raise_for_status()
         )
         yield from fetch(session, start_date=DATE, end_date=DATE)
-        
-        
+
+
 if __name__ == "__main__":
     output_path = PATH / f"{DATE}.csv"
     output_path.unlink(missing_ok=True)
     data = fetch_all()
     for i, dataframe in enumerate(data):
         write_header = True if i == 0 else False
-        dataframe.to_csv(
-            output_path, header=write_header, index=False, mode="a"
-        )
+        dataframe.to_csv(output_path, header=write_header, index=False, mode="a")
